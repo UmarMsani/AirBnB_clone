4a89b6ac3421bf7cf563bea1e535ab25306b5201
Nonsitech <samleeu01@gmail.com>
2023-10-15 08:12:47 -0700

    city.py

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+City class module
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    Class for City objects, inherits from BaseModel.
+    """
+
+    state_id = ""
+    name = ""
+
+
