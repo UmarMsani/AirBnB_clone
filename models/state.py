0719e7f281a39cf262bdc3c275612ac00a6e0a7a
Nonsitech <samleeu01@gmail.com>
2023-10-15 08:19:31 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+State class module
+"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    Class for State objects, inherits from BaseModel.
+    """
+    name = ""
+
+
